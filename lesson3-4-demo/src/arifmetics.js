c6b39d4c73c3f9189b64cf0928ee7aee261102c7
SvIlina <svitlana.ilina@gmail.com>
2026-03-15 20:52:39 +0100

    lesson3

---
+++ lesson3-4-demo/src/arifmetics.js
@@ -0,0 +1,14 @@
+
+const number1 = 10;
+const number2 = 51;
+const v1 ='spring'
+const v2 = 'summer'
+console.log(number1 + v1) 
+console.log(number1 - v1)
+
+const spring= true
+const emp = null
+console.log(spring/v1)
+console.log(emp*v1)
+console.log(number1*number2)
+console.log(`${v1} + ${v2}`)
---
+++ lesson3-4-demo/src/decision-tree.js
@@ -0,0 +1,10 @@
+const age = 18
+    if (age < 18) {
+        console.log('You are not allowed to buy alcohol')
+    } else if (age >= 18 && age < 65) {
+        console.log('You are allowed to buy alcohol')
+    } else {
+        console.log('Drink tea and stay healthy')
+    };
+
+
