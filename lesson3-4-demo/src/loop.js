4d24dd05e8f2ec94db5f3a3e06b1b8779580cb82
SvIlina <svitlana.ilina@gmail.com>
2026-03-18 16:54:11 +0100

    lesson3

--- lesson3-4-demo/src/loop.js
+++ lesson3-4-demo/src/loop.js
@@ -2,14 +2,15 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
 } // This will print numbers from 0 to 9
 
-while (i>=0 && i<10) {
+let i = 0;
+while (i >= 0 && i < 10) {
     console.log(i);
 } // This will print numbers from 0 to 9
 
 do {
     console.log(i);
     i++;
-} while (i>=0 && i<10) // This will print numbers from 0 to 9
+} while (i >= 0 && i < 10); // This will print numbers from 0 to 9
 
 for (let i = 100; i > 0; i -= 10) {
     console.log(i);
@@ -23,4 +24,4 @@ while (i > 0) {
 do {
     console.log(i);
     i -= 10;
-} while (i > 0)// This will print numbers from 100 to 10, decrementing by 10 each time printing: 100, 90, 80, 70, 60, 50, 40, 30, 20, 10
+} while (i > 0);// This will print numbers from 100 to 10, decrementing by 10 each time printing: 100, 90, 80, 70, 60, 50, 40, 30, 20, 10
--- lesson3-4-demo/src/obj.js
+++ lesson3-4-demo/src/obj.js
@@ -11,8 +11,8 @@ const fruits = {
     func: function() {
         console.log('This is a function inside the fruits object');
     }
-}
+};
 
-Object.keys(fruits)// ['name', 'apple', 'banana', 'func']
-Object.values(fruits)
-Object.entries(fruits)
+Object.keys(fruits);// ['name', 'apple', 'banana', 'func']
+Object.values(fruits);// [['apple', 'banana', 'orange', 'grape', 'pear'], { color: 'red', taste: 'sweet' }, { color: 'yellow', taste: 'sweet' }, function()]
+Object.entries(fruits);// [['name', ['apple', 'banana', 'orange', 'grape', 'pear']], ['apple', { color: 'red', taste: 'sweet' }], ['banana', { color: 'yellow', taste: 'sweet' }], ['func', function() { console.log('This is a function inside the fruits object'); }]]
