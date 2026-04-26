cb76edbbe5b4bff9a6200de7bcce5f9651ea5082
SvIlina <svitlana.ilina@gmail.com>
2026-04-26 22:12:51 +0200

    lesson7_HW

---
+++ lesson-7-HW/src/arrow-functions.ts
@@ -0,0 +1,11 @@
+
+const letters: string[] = ["K", "B", "C", "D", "L"];
+
+const integ: number[] = [1, 2, 3, 4, 5];
+
+const tst = (levels: string[], integers: number[]): void  => {
+    console.log(levels);
+    console.log(integers);
+};
+
+tst(letters, integ);
