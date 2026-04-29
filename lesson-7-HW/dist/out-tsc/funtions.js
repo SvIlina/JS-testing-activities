2dbc57d499b6b32c67e8e0942d400da2fc977a11
SvIlina <svitlana.ilina@gmail.com>
2026-04-29 21:28:10 +0200

    corrected branch

--- lesson-7-HW/dist/out-tsc/funtions.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const numbers = [1.2, 0.3, 4.2, 1.3, 2.43];
-const initialValue = 0;
-const sumWithInitial = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
-console.log(sumWithInitial); // 9.13
-//# sourceMappingURL=funtions.js.map
